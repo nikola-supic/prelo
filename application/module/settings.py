fdf683a882672991fdbe7a4863a73651fb054eaa
just_suco <nikola.supic09@gmail.com>
2021-07-13 12:27:48 +0200

    basic settings UI

---
+++ application/module/settings.py
@@ -0,0 +1,36 @@
+# Importing the libraries
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtCore import QPropertyAnimation, QSequentialAnimationGroup
+from ui.screen_settings import Ui_SettingsScreen
+
+from datetime import datetime
+
+
+# SETTINGS SCREEN
+class SettingsScreen(QMainWindow, Ui_SettingsScreen):
+    def __init__(self, last_screen, user):
+        super(SettingsScreen, self).__init__()
+        self.setupUi(self)
+        self.back = last_screen
+        self.user = user
+
+        # Remove title bar
+        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+
+        # Events
+        self.btn_back.clicked.connect(self.exit)
+        self.click_time = get_time() + 2
+
+        self.show()
+
+
+    def exit(self):
+        self.back.show()
+        self.close()
+
+
+def get_time():
+    return datetime.now().timestamp()
