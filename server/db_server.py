5bc6dae9551d7e330e6f4060276a6a55942332a7
just_suco <nikola.supic09@gmail.com>
2021-07-15 19:24:30 +0200

    added artist table

--- server/db_server.py
+++ server/db_server.py
@@ -28,9 +28,22 @@ def connect(host='localhost', user='root', password='', database=''):
 
 # songs-related functions
 def add_song(artist, name, path, length, added_by):
-    sql = "INSERT INTO song (artist, name, path, length, added_by) VALUES (%s, %s, %s, %s, %s)"
-    val = (artist, name, path, length, added_by, )
+    sql = "SELECT * FROM artist WHERE name=%s"
+    val = (artist, )
+    mycursor.execute(sql, val)
+    result = mycursor.fetchone()
 
+    if result is None:
+        sql = "INSERT INTO artist (name) VALUES (%s)"
+        val = (artist, )
+        mycursor.execute(sql, val)
+        mydb.commit()
+        artist_id = mycursor.lastrowid
+    else:
+        artist_id = result[0]
+
+    sql = "INSERT INTO song (artist_id, name, path, length, added_by) VALUES (%s, %s, %s, %s, %s)"
+    val = (artist_id, name, path, length, added_by, )
     mycursor.execute(sql, val)
     mydb.commit()
     return mycursor.lastrowid
